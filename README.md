c9e599a98c8dd12fd1c40b35ede5caf8912c0cad
jacobptak2005 <jacobptak2005@gmail.com>
2023-04-04 12:24:08 +1200

    Initial 2

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ScholarshipProj2023
-/Users/jacobptak/Documents/GitHub/ScholarshipProj2023/README.md
--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>') 
