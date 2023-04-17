20c25ac524e37259ed6aad9d5370ab7ef6d49c3e
jacobptak2005 <jacobptak2005@gmail.com>
2023-04-17 20:02:00 +1000

    nav create 1

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'home/home.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1>') 
