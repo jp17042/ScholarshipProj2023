ce3b44a23d9eb42e98a5321f024a1188be8fba29
jacobptak2005 <jacobptak2005@gmail.com>
2023-05-04 14:53:42 +1200

    Fixing cannot find template error
    under django_project/settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+STATICFILES_DIRS = ['/Users/jacobptak/Documents/GitHub/ScholarshipProj2023/home/static']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'signup.apps.SignupConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap_icons',
 ]
 
 MIDDLEWARE = [
