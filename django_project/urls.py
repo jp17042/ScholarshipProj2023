ddb1daa8e81d1b6ed34251606eec983092cc3f71
jacobptak2005 <jacobptak2005@gmail.com>
2023-05-11 20:54:12 +1200

    login final
    Create an sqlite interface that sent to from html form to the sqlite database

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,11 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< Updated upstream
+
     path('blog/', include('blog.urls')),
-=======
     path('', include('home.urls')),
     path('signup/', include('signup.urls')),
     path('login/', include('login.urls'))
->>>>>>> Stashed changes
+
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import InfoPersonal
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(name, email, password)
+
+        new_info = InfoPersonal()
+        new_info.name = name
+        new_info.email = email
+        new_info.password = password
+
+
+        new_info.save()
+
+
     return render(request, 'login/login.html')
+
+
