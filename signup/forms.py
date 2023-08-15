c9a7eed10013eed3522e863c8a4909bfeff9886e
jp17042 <103467624+jp17042@users.noreply.github.com>
2023-08-15 17:09:46 +1200

    Fixing commit error

--- signup/forms.py
+++ signup/forms.py
@@ -4,3 +4,4 @@ class SignupForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
+    last_name = forms.CharField(max_length=150)
