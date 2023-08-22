8349f0a221db21b386bb724240bc7943d1416250
jp17042 <103467624+jp17042@users.noreply.github.com>
2023-08-22 18:43:02 +1200

    csv initial

--- signup/forms.py
+++ signup/forms.py
@@ -4,4 +4,3 @@ class SignupForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
-    last_name = forms.CharField(max_length=150)
